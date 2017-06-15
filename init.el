b1bafb5278b39a6768be326c17d0d4cd1b029687
Zhenqiang Li <lizhenqiangsnake@gmail.com>
2017-06-15 07:53:16 -0400

    change some keyshort

--- init.el
+++ init.el
@@ -1,3 +1,24 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "~/.emacs.d/ryne-emacs-init.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (go-mode python-mode sr-speedbar company-quickhelp company-flx company rainbow-delimiters leuven-theme gnuplot-mode gnuplot helm-projectile projectile smex flx-ido ido-ubiquitous helm-bibtex helm-gtags helm magit auctex use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
